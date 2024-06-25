926233ce7c9a0b4eee43e064174f97ea5fd127f9
Kanish-Sk <kanishshivan@gmail.com>
2024-06-25 23:27:45 +0530

    footer link update

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -3,25 +3,40 @@ import "./footer.css";
 const Footer = () => {
   return (
     <div className="bg-gray-800 w-full h-16 md:h-20 rounded-2xl p-2 flex items-center justify-center bottom-0">
-      <ul>
+      <ul className="flex space-x-4">
         <li className="item">
-          <a href="#">
+          <a href="mailto:kanishshivan@gmail.com" aria-label="Email">
             <i className="fas fa-envelope logo"></i>
           </a>
         </li>
         <li className="item">
-          <a href="#">
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
+          >
             <i className="fab fa-instagram logo"></i>
           </a>
         </li>
         <li className="item">
-          <a href="#">
+          <a
+            href="https://www.linkedin.com/in/kanish-s-1446a2247"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+          >
             <i className="fab fa-linkedin logo"></i>
           </a>
         </li>
         <li className="item">
-          <a href="#">
-            <i class="fa-brands fa-x-twitter logo"></i>
+          <a
+            href="https://x.com/home?lang=en"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Twitter"
+          >
+            <i className="fa-brands fa-x-twitter logo"></i>
           </a>
         </li>
       </ul>
